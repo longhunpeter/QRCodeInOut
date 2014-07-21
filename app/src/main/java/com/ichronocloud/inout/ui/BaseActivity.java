9fdf0406faedd43fa1f2c268f242ccc42180496c
longhunpeter <645142803peter@gmail.com>
2014-07-21 13:18:20 +0800

    Update BaseActivity.java
    修改测试

--- app/src/main/java/com/ichronocloud/inout/ui/BaseActivity.java
+++ app/src/main/java/com/ichronocloud/inout/ui/BaseActivity.java
@@ -26,7 +26,7 @@ public class BaseActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //test
+        //test name
     }
 
 
